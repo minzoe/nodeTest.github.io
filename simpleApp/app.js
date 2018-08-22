b8cfa55beec5fd6b462702518d17679dacdb2ab5
Miner <zminer@byui.edu>
2018-08-21 18:36:03 -0600

    update

--- simpleApp/app.js
+++ simpleApp/app.js
@@ -2,6 +2,11 @@ const http = require('http');
 const express = require('express');
 const router = require('./router.js');
 var querystring = require("querystring");
+const ApiCall = require("./api.js");
+var render = require("./render");
+var commonHeader = {
+  'Content-Type': 'html'
+};
 
 const port = 3000;
 
@@ -10,19 +15,44 @@ app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  router.home(req, res);
-}).post('/', (req, res) => {
-  req.on("data", function (postBody) {
-    var query = querystring.parse(postBody.toString());
-    res.writeHead(303, {
-      "location": "/search?type=" + query.type + "&value=" + query.qVal
+    router.home(req, res);
+  }).post('/', (req, res) => {
+    req.on("data", function (postBody) {
+      var query = querystring.parse(postBody.toString());
+      res.writeHead(303, {
+        "location": "/search?type=" + query.type + "&value=" + query.qVal
+      });
+      res.end();
+    })
+  }).get('/search', (req, res) => {
+    var type = req.query.type;
+    var value = req.query.value;
+    var api = new ApiCall(type, value)
+    api.on('end', function (data) {
+      res.render('profile', {
+        results: data.results
+      });
     });
-    res.end();
+  }).get('/test', (req, res) => {
+    var drinks = [{
+        name: 'Bloody Mary',
+        drunkness: 3
+      },
+      {
+        name: 'Martini',
+        drunkness: 5
+      },
+      {
+        name: 'Scotch',
+        drunkness: 10
+      }
+    ];
+    res.render('profile', {
+      results: drinks,
+      title: "This is a Test"
+    });
   })
-}).get('/search', (req, res) => {
-  router.spell(req, res);
-})
 
-.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+  .listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
--- simpleApp/router.js
+++ simpleApp/router.js
@@ -1,39 +1,42 @@
 const ApiCall = require("./api.js");
 var render = require("./render");
-const handlebars = require('handlebars');
-var commonHeader = { 'Content-Type': 'html' };
+var ejs = require("ejs");
+var commonHeader = {
+  'Content-Type': 'html'
+};
 
 function home(req, res) {
-      res.writeHead(200, commonHeader);
-      render.view("header", {}, res);
-      render.view("navigation", {}, res);
-      render.view("search", {}, res);
-      render.view("footer", {}, res);
-      res.end();
+  res.writeHead(200, commonHeader);
+  render.view("header", {}, res);
+  render.view("navigation", {}, res);
+  render.view("search", {}, res);
+  render.view("footer", {}, res);
+  res.end();
 }
 
 function spell(req, res) {
   var type = req.query.type;
   var value = req.query.value;
-    var api = new ApiCall(type, value)
-    api.on('end', function (data) {
-      var values = {
-        avatarUrl: data.results[0].url,
-        username: data.results[0].name,
-        results: data.results
-        // badges: data.badges.length,
-        // javascript: data.points.JavaScript
+  var api = new ApiCall(type, value)
+  api.on('end', function (data) {
+    var results = data.results;
+    return new Promise((res, rej) => {
+      if (err) {
+        rej(err);
+      } else {
+        res(results);
       }
-      var results = data.results[0];
-      console.log(results);
-      res.writeHead(200, commonHeader);
-      render.view("header", {}, res);
-      render.view("navigation", {}, res);
-      // render.ejs("profile", values, res);
-      res.render('profile');
-      render.view("footer", {}, res);
-      res.end();
-    });
+    })
+    // res.writeHead(200, commonHeader);
+    // render.view("header", {}, res);
+    // render.view("navigation", {}, res);
+    // render.ejs("profile", values, res);
+    // res.render('profile', {
+    //   results: results
+    // });
+    // render.view("footer", {}, res);
+    res.end();
+  });
 
   //   studentProfile.on('error', function (err) {
   //     render.view("header", {}, res);
