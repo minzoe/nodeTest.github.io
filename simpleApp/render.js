195406633a4146388ab29ad557481f363d4bb4c8
Miner <zminer@byui.edu>
2018-09-18 13:50:42 -0600

    update

--- simpleApp/render.js
+++ simpleApp/render.js
@@ -8,13 +8,13 @@ function mergeValues(values, content) {
 }
 
 function html(templateName, values, res) {
-  var fileContent = fs.readFileSync('./views/' + templateName + '.html', 'utf8');
+  var fileContent = fs.readFileSync('./views/partials/' + templateName + '.html', 'utf8');
   fileContent = mergeValues(values, fileContent);
   res.write(fileContent);
 }
 
 function ejs(templateName, values, res) {
-  var fileContent = fs.readFileSync('./views/' + templateName + '.ejs', 'utf8');
+  var fileContent = fs.readFileSync('./views/partials/' + templateName + '.ejs', 'utf8');
   fileContent = mergeValues(values, fileContent);
   res.write(fileContent);
 }
